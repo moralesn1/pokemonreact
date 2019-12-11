b57aa1d9d5519a27b042efcb7f60dfe15bbbc749
Nathaniel <nathanielmorales01@gmail.com>
2019-12-11 15:56:48 +1100

    add pagination button, loading and ternary operators for pagination checks

--- src/App.js
+++ src/App.js
@@ -1,18 +1,56 @@
 import React, { useState, useEffect } from "react";
 import PokemonList from "./PokemonList";
 import axios from "axios";
+import Pagination from "./Pagination";
 
 function App() {
   const [pokemon, setPokemon] = useState([]);
+  const [currentPageUrl, setCurrentPageUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon"
+  );
+  const [nextPageUrl, setNextPageUrl] = useState();
+  const [prevPageUrl, setPrevPageUrl] = useState();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios.get("https://pokeapi.co/api/v2/pokemon").then(res => {
-      setPokemon(res.data.results.map(p => p.name));
-    });
-  }, []);
-  // Effect only renders once.
+    setLoading(true);
+    let cancel;
+    axios
+      .get(currentPageUrl, {
+        cancelToken: new axios.CancelToken(c => (cancel = c))
+      })
+      .then(res => {
+        setLoading(false);
+        setNextPageUrl(res.data.next);
+        setPrevPageUrl(res.data.previous);
+        setPokemon(res.data.results.map(p => p.name));
+      });
 
-  return <PokemonList pokemon={pokemon} />;
+    return () => cancel();
+    // cancels any previous requests sent everytime a new request is made
+  }, [currentPageUrl]);
+
+  // Everytime currentPageURL changes, useEffect will refetch and rerender; if it doesn't change
+  // It won't rerender
+
+  function goToNextPage() {
+    setCurrentPageUrl(nextPageUrl);
+  }
+
+  function goToPrevPage() {
+    setCurrentPageUrl(prevPageUrl);
+  }
+
+  if (loading) return "Loading...";
+  return (
+    <>
+      <PokemonList pokemon={pokemon} />
+      <Pagination
+        goToNextPage={nextPageUrl ? goToNextPage : null}
+        goToPrevPage={prevPageUrl ? goToPrevPage : null}
+      />
+    </>
+  );
 }
 
 export default App;
